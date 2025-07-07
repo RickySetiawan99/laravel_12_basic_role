baeb38e55cb4f4d6a48c643b77907741d83b812d
Ricky Setiawan <rickiricki201533@gmail.com>
2025-07-07 09:25:52 +0700

    fix logo title

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,9 +10,29 @@
   href="{{ asset('assets') }}/images/logos/favicon.png"
 />
 
+<title>
+  @if (isset($title))
+    {{ $title }}
+  @else
+    {{ config('app.name', 'Brillo Asset Management') }}
+  @endif
+</title>
+
 <!-- Core Css -->
 <link rel="stylesheet" href="{{ asset('assets') }}/css/styles.css" />
 
 <link rel="stylesheet" href="{{ asset('assets') }}/libs/datatables.net-bs5/css/dataTables.bootstrap5.min.css" />
 <link rel="stylesheet" href="{{ asset('assets') }}/libs/sweetalert2/dist/sweetalert2.min.css">
 <link rel="stylesheet" href="{{ asset('assets') }}/libs/select2/dist/css/select2.min.css">
+<style>
+  @media (prefers-color-scheme: dark) {
+      .light-logo {
+          display: none;
+      }
+  }
+  @media (prefers-color-scheme: light) {
+      .dark-logo {
+          display: none;
+      }
+  }
+</style>
